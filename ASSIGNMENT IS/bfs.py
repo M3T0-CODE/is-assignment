a8c73cd46878d79e57f4fb3a9fb9045679afb3d5
M3T0-CODE <115326047+M3T0-CODE@users.noreply.github.com>
2024-10-31 11:33:11 +0300

    Add files via upload

---
+++ ASSIGNMENT IS/bfs.py
@@ -0,0 +1,15 @@
+def bfs(graph, start , goal):
+    queue = [[start]]
+    while queue:
+       path = queue.pop(0)
+       node = path [-1]
+       if node == goal:
+          return path
+       else:
+          adjacent_nodes = graph.get(node, [])
+          for node2 in adjacent_nodes:
+             if node2 not in path:
+                new_path = list(path)
+                new_path.append(node2)
+                queue.append(new_path) 
+ 
